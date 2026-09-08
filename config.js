95e762f53152abea8c27a8dbf8ea6632e514d454
TechyWecky69 <techyisontop@gmail.com>
2026-09-08 22:24:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // BlockBound Store/API configuration.
 // When the website and API are hosted together, leave this as an empty string.
 window.BLOCKBOUND_CONFIG = {
-  storeApi: "",
+  storeApi: "http://51.155.195.251:5001",
   discordApi: ""
 };
