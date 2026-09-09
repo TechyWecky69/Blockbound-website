55ea837a50aaa5f8b0e1ce704cdff72897806eca
TechyWecky69 <techyisontop@gmail.com>
2026-09-09 16:25:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // BlockBound Store/API configuration.
 window.BLOCKBOUND_CONFIG = {
-  storeApi: "https://store-api.block-bound.org",
-  discordApi: ""
+  storeApi: "https://store-api.block-bound.org"
 };
